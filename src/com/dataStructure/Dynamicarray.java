3458b9fe418f7b96dc321f3f118b258cb0a5fc94
lalit527 <lalit.slg007@gmail.com>
2017-12-10 17:28:25 +0530

    Initial Version

---
+++ src/com/dataStructure/Dynamicarray.java
@@ -0,0 +1,7 @@
+package com.dataStructure;
+
+public class Dynamicarray {
+    public Dynamicarray() {
+        System.out.println("Hello Again");
+    }
+}
---
+++ src/com/dataStructure/Main.java
@@ -0,0 +1,10 @@
+package com.dataStructure;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Dynamicarray a = new Dynamicarray();
+        System.out.println("Hello Worlds");
+    }
+}
