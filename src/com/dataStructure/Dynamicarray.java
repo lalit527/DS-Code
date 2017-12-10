0dbbb93b714d1ea8c3e5771a5403cff5a6ec03ec
lalit527 <lalit.slg007@gmail.com>
2017-12-10 18:47:53 +0530

    Dynamic Array

--- src/com/dataStructure/Dynamicarray.java
+++ src/com/dataStructure/Dynamicarray.java
@@ -1,7 +1,51 @@
 package com.dataStructure;
+import java.lang.Exception;
 
 public class Dynamicarray {
+    private static final int INCREASE_FACTOR = 2;
+    private int capacity;
+    private int count;
+    private Object[] data;
+
     public Dynamicarray() {
-        System.out.println("Hello Again");
+        /*System.out.println("Hello Again");*/
+        this.capacity = 1;
+        this.count = 0;
+        this.data = new Object[this.capacity];
     }
+
+    public int getLength(){
+        return  this.count;
+    }
+
+    public void addElement(Object obj){
+        if(this.capacity == this.count){
+            increaseSize(obj);
+        }
+        this.data[this.count] = obj;
+        ++this.count;
+    }
+
+    public void increaseSize(Object obj){
+        this.capacity = INCREASE_FACTOR * this.capacity;
+        Object tmpData[] = new Object[this.capacity];
+        for (int i=0; i<this.count; i++) {
+             tmpData[i] = this.data[i];
+        }
+        this.data = tmpData;
+
+    }
+
+    public int getCapacity(){
+        return this.capacity;
+    }
+
+    public Object getElement(int index) throws Exception{
+        if (index >=count || index < 0){
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+        return this.data[index];
+    }
+
+
 }
--- src/com/dataStructure/Main.java
+++ src/com/dataStructure/Main.java
@@ -5,6 +5,19 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         Dynamicarray a = new Dynamicarray();
-        System.out.println("Hello Worlds");
+        a.addElement("1");
+        a.addElement("2");
+        a.addElement("3");
+        a.addElement("4");
+        a.addElement("5");
+        System.out.println(a.getLength());
+        System.out.println(a.getCapacity());
+        try{
+            System.out.println(a.getElement(5));
+        }catch(Exception e){
+            System.out.println("Exception Occured"+e);
+        }
+
+        
     }
 }
