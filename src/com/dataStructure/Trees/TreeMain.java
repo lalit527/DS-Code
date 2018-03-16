abc99228562be54c514b312323ff3f251f542ae0
lalit527 <lalit.slg007@gmail.com>
2018-03-16 14:42:17 +0530

    Initialized the project

--- src/com/dataStructure/Trees/TreeMain.java
+++ src/com/dataStructure/Trees/TreeMain.java
@@ -1,5 +1,4 @@
 package com.dataStructure.Trees;
-import com.dataStructure.Trees.BinaryTree;
 public class TreeMain {
     public static void main(String[] args){
         /*int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
